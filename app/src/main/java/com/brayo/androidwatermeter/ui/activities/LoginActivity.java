2ba25297bdbce2829e33ff61a059e28dfe99af69
jumaBrian <njengabrianj@gmail.com>
2022-07-20 17:05:36 +0300

    feat: Add Onboarding navigation

--- app/src/main/java/com/brayo/androidwatermeter/ui/activities/LoginActivity.java
+++ app/src/main/java/com/brayo/androidwatermeter/ui/activities/LoginActivity.java
@@ -1,16 +1,66 @@
 package com.brayo.androidwatermeter.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.brayo.androidwatermeter.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmailLogin)
+    EditText userEmailLogin;
+    @BindView(R.id.etPasswordLogin)
+    EditText userPasswordLogin;
+    @BindView(R.id.bvLogin)
+    Button login;
+    @BindView(R.id.bv_create_account)
+    Button createAccount;
+    @BindView(R.id.bv_forgot_password)
+    Button forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        // Variables
+
+
+        // button navigation
+        // 1. Log in button
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 2. Create account button
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        // 3. Forgot password button
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/brayo/androidwatermeter/ui/activities/SignupActivity.java
+++ app/src/main/java/com/brayo/androidwatermeter/ui/activities/SignupActivity.java
@@ -1,16 +1,59 @@
 package com.brayo.androidwatermeter.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.brayo.androidwatermeter.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignupActivity extends AppCompatActivity {
+    @BindView(R.id.edStaffID)
+    EditText staffID;
+    @BindView(R.id.edEmailNew)
+    EditText newAccountEmail;
+    @BindView(R.id.edPasswordNew)
+    EditText newAccountPassword;
+    @BindView(R.id.bvRegister)
+    Button registerUser;
+    @BindView(R.id.bvHome)
+    Button returnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        // Variables
+
+
+        // Button Navigation
+        // 1. Register User
+        registerUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 2. Abort
+        returnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LauncherActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
